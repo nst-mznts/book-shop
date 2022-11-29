f3916ce251314038f29ed5599e6ba8c9ba89b782
Anastasiia <middlenast@gmail.com>
2022-11-29 22:14:48 +0300

    refactor: change the bag algorithm

--- catalog/script.js
+++ catalog/script.js
@@ -79,11 +79,9 @@ const books = [{
       "description": "Secrets of the Javascript Ninja takes you on a journey towards mastering modern JavaScript development in three phases: design, construction, and maintenance. Written for JavaScript developers with intermediate-level skills, this book will give you the knowledge you need to create a cross-browser JavaScript library from the ground up."
     }
 ]
-const body = document.body;
+var basket = {}; // Basket
 
-var basket = {};
-var sum = 0;
-checkBasket();
+const body = document.body;
 
 const header = document.createElement('header');
 body.appendChild(header);
@@ -110,73 +108,6 @@ logoContainer.appendChild(logo);
 
 const main = document.createElement('main');
 body.appendChild(main);
-
-const openbtn = document.createElement('button');
-openbtn.innerHTML = 'Bag';
-openbtn.classList.add('openbtn');
-main.appendChild(openbtn);
-
-const sidebar = document.createElement('div');
-sidebar.classList.add('sidebar');
-sidebar.id = 'mySidebar';
-main.appendChild(sidebar);
-
-const closebtn = document.createElement('button');
-closebtn.innerHTML = 'Close';
-closebtn.classList.add('closebtn');
-sidebar.appendChild(closebtn);
-
-const openNav = () => {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-};
-  
-const closeNav = () => {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "auto";
-};
-  
-openbtn.addEventListener("click", openNav);
-closebtn.addEventListener("click", closeNav);
-
-function showBasket(){
-    for (let key in basket) {
-        let miniCard = document.createElement('div');
-        miniCard.classList.add('miniCard');
-        miniCard.id = basket[key];
-        let bookImg = document.createElement('img');
-        bookImg.src = books[key].imageLink;
-        miniCard.appendChild(bookImg);
-        let textWrapper = document.createElement('div');
-        miniCard.appendChild(textWrapper);
-        let bookAuthor = document.createElement('h4');
-        bookAuthor.innerHTML = books[key].author;
-        textWrapper.appendChild(bookAuthor);
-        let bookTitle = document.createElement('p');
-        bookTitle.innerHTML = books[key].title;
-        textWrapper.appendChild(bookTitle);
-        let countWrapper = document.createElement('div');
-        countWrapper.classList.add('countWrapper');
-        textWrapper.appendChild(countWrapper);
-        /*let minus = document.createElement('button');
-        minus.innerHTML = '-';
-        countWrapper.appendChild(minus);*/
-        let price = document.createElement('p');
-        price.innerHTML = '$' + books[key].price;
-        countWrapper.appendChild(price);
-        sum += books[key].price;
-        /*let plus = document.createElement('button');
-        plus.innerHTML = '+';
-        countWrapper.appendChild(plus);*/
-        let closeBtn = document.createElement('button');
-        closeBtn.innerHTML = 'X';
-        closeBtn.classList.add('delete-item');
-        miniCard.appendChild(closeBtn);
-        sidebar.appendChild(miniCard);
-    }
-
-            
-}
 
 const wrapper = document.createElement('div');
 wrapper.classList.add('main-wrapper');
@@ -262,42 +193,6 @@ closePopupButton.addEventListener('click',() => {
 
 
 
-//showBacket();
-/*document.querySelector('#main').addEventListener('click', event => {
-
-    if (event.target.classList.contains('to-bag')) {
-        let idItem = event.target.id;
-        if (backet[idItem] !== undefined) {
-            backet[idItem]++;
-        }
-        else {
-            backet[idItem] = 1;
-            let miniCard = document.createElement('div');
-            miniCard.classList.add('miniCard');
-            miniCard.id = idItem;
-            let bookImg = document.createElement('img');
-            bookImg.src = books[idItem].imageLink;
-            miniCard.appendChild(bookImg);
-            let textWrapper = document.createElement('div');
-            miniCard.appendChild(textWrapper);
-            let bookAuthor = document.createElement('p');
-            bookAuthor.innerHTML = books[idItem].author;
-            textWrapper.appendChild(bookAuthor);
-            let bookTitle = document.createElement('p');
-            bookTitle.innerHTML = books[idItem].title;
-            textWrapper.appendChild(bookTitle);
-            let closeBtn = document.createElement('button');
-            closeBtn.innerHTML = 'X';
-            closeBtn.classList.add('delete-item');
-            closeBtn.id = idItem;
-            miniCard.appendChild(closeBtn);
-            sidebar.appendChild(miniCard);
-        }
-        localStorage.setItem('backet', JSON.stringify(backet));
-        console.log(backet);        
-    }
-});*/
-
 function addToBacket(event) {
     //добавляем товар в корзину
     if (event.target.classList.contains('to-bag')) {
@@ -309,21 +204,10 @@ function addToBacket(event) {
             basket[idItem] = 1;
         }
     }
-    localStorage.setItem('basket', JSON.stringify(basket) );
-    showBasket();
-    let total = document.createElement('p');
-    total.innerHTML = '$' + sum;
-    sidebar.appendChild(total);
-    
+    localStorage.setItem('basket', JSON.stringify(basket) );    
 }
 
-function checkBasket() {
-    if (localStorage.getItem('basket') !== null) {
-        basket = JSON.parse(localStorage.getItem('basket'));
-    }
-}
-
-const confirmBtn = document.createElement('button');
+/*const confirmBtn = document.createElement('button');
 confirmBtn.innerHTML = 'Confirm order';
 confirmBtn.classList.add('confirm');
 sidebar.appendChild(confirmBtn);
@@ -334,4 +218,4 @@ makeOrder.addEventListener('click',(e) => {
     e.preventDefault();
     document.querySelector(".form__bg").classList.add('active');
     document.querySelector(".form").classList.add('active');
-});
+});*/
